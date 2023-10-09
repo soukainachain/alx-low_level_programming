006140f91cceee68205bd9911fb30a132d296af2
soukianachain <soukainachain>
2023-10-09 10:59:43 -0700

    a

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,11 +5,11 @@
  *
  * Description: A C program that prints with put function 
  *
- * Return: Alawys 0 (Success)
+ * Return: Alawys 0(Success)
 */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);	    
+	return (0);
 }
