c591d21d67634647b42c72bc2a66d56397326a85
soukianachain <soukainachain>
2023-10-10 10:02:59 -0700

    a

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,12 +2,13 @@
 
 /**
  * print_alphabet - utilizes on the _putchar function to print
- *                   the alphabet a - z
+ *                 the alphabet a - z
 */
 
 void print_alphabet(void)
 {
 	int ch;
+
 	for (ch = 'a'; ch <= 'z'; ch++)
 		_putchar(ch);
 	_putchar('\n');
