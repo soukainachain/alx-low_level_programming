f14c248e6d8a11c40cc8fa72fa39e5483422adf3
soukianachain <soukainachain>
2023-10-11 07:55:16 -0700

    a

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,10 +2,10 @@
 
 /**
  * _islower - function to chech if
- *           character is lowercase
+ *           caracter is lowercase
  *
  * 0c: checks input of function
- *  
+ *
  * Return: returns 1 if 'c' is lowercase
  *         otherwise always 0 (Success)
 */
@@ -13,6 +13,6 @@
 int  _islower(int c)
 {
 	if (c >= 97 && c <= 122)
-	        return (1);
+		return (1);
 	return (0);
 }
