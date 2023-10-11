10b6a13ca59c44a0916f46baeec500453533a98c
soukianachain <soukainachain>
2023-10-11 08:21:42 -0700

    a

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,5 +1,14 @@
 #include "main.h"
 
+/**
+ * _isalpha check if character is a letter
+ *          both lowercase or uppercase
+ *
+ * 0c: takes input from other function
+ *
+ * Return: 1 is c if true else 0
+*/
+
 int _isalpha(int c)
 {
 	if (c >= 97 && c <= 122)
